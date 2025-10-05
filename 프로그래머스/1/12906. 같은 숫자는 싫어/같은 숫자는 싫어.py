b7325675f4df996fddceb03d38f667c0cf577dae
Jiwoo <164865998+Jiwoo-Leee@users.noreply.github.com>
2025-10-05 21:14:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 197.61 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i+1 >= len(arr):
+            break
+        if arr[i] != arr[i+1]:
+            answer.append(arr[i])
+    answer.append(arr[-1])
+    return answer
