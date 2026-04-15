18edf8f778f983a1285d9e40c26015f0ab325bcf
Jiwoo <164865998+Jiwoo-Leee@users.noreply.github.com>
2026-04-15 14:37:08 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,13 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    for h in range(3, total+1):
+        if total % h != 0:
+            continue
+        w = total // h
+        
+        if w < h:
+            continue
+            
+        if (w - 2) * (h - 2) == yellow:
+            return [w, h]
+
